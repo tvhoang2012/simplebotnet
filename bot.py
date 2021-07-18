0ef406f0a01d7087dfa1378cb66f724b893fc0b0
Hoàng Tạ Việt <18520060@ms.uit.edu.vn>
2021-07-18 22:39:48 +0700

    test

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ def at_converter(message):
     if at_text == '@': # in case it's just the '@', skip
         pass
     else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
+        insta_link = "https://instagram.com/{}".format(at_text[1:]) # find instagram of someone
         bot.reply_to(message, insta_link)
 @bot.message_handler(func=lambda msg: msg.text is not None and 'download' in msg.text) # message download file 
 def at_converter(message):
